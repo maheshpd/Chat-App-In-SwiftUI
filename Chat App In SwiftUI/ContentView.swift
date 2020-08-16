0a02b568123b823da8213f4e83aa0078c2d98ca2
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-08-16 10:57:55 +0530

    Part 1

--- Chat App In SwiftUI/ContentView.swift
+++ Chat App In SwiftUI/ContentView.swift
@@ -8,14 +8,51 @@
 
 import SwiftUI
 
+
+//https://console.firebase.google.com/u/0/project/blogpost-f878a/overview
 struct ContentView: View {
+    
+    @State var name = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            ZStack{
+                Color.orange
+                
+                VStack{
+                    Image(systemName: "person.circle.fill").resizable()
+                        .frame(width: 60, height: 60).padding(.top, 12)
+                    
+                    TextField("Name", text: $name).textFieldStyle(RoundedBorderTextFieldStyle()).padding()
+                    
+                    NavigationLink(destination: MsgPage()){
+                        HStack {
+                            Text("Join")
+                            Image(systemName: "arrow.right.circle.fill")
+                                .resizable().frame(width: 20, height: 20)
+                        }
+                    }.frame(width: 100, height: 54)
+                        .background(Color.orange)
+                        .foregroundColor(.white)
+                    .cornerRadius(27)
+                        .padding(.bottom, 15)
+                }.background(Color.white)
+                .cornerRadius(20)
+            .padding()
+            }.edgesIgnoringSafeArea(.all)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct MsgPage : View {
+    var name = ""
+    var body: some View {
+        Text("hello")
     }
 }
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Chat App In SwiftUI' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Chat App In SwiftUI
+
+pod 'Firebase/Firestore' 
+	
+
+end
