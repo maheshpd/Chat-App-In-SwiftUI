fb4969f1628dce32b70bbc3009268ea968817896
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-08-17 13:41:53 +0530

    Chat App In SwiftUI - Part 2

--- Chat App In SwiftUI/ContentView.swift
+++ Chat App In SwiftUI/ContentView.swift
@@ -7,7 +7,7 @@
 //
 
 import SwiftUI
-
+import Firebase
 
 //https://console.firebase.google.com/u/0/project/blogpost-f878a/overview
 struct ContentView: View {
@@ -25,7 +25,7 @@ struct ContentView: View {
                     
                     TextField("Name", text: $name).textFieldStyle(RoundedBorderTextFieldStyle()).padding()
                     
-                    NavigationLink(destination: MsgPage()){
+                    NavigationLink(destination: MsgPage(name: self.name)){
                         HStack {
                             Text("Join")
                             Image(systemName: "arrow.right.circle.fill")
@@ -52,7 +52,81 @@ struct ContentView_Previews: PreviewProvider {
 
 struct MsgPage : View {
     var name = ""
+    @ObservedObject var msg = observer()
+    @State var typedmsg = ""
+    
+    
     var body: some View {
-        Text("hello")
+        VStack{
+            List(msg.msgs){i in
+                Text(i.msg)
+            }.navigationBarTitle("Chats",displayMode: .large)
+            
+            HStack {
+                
+                TextField("Msg", text: $typedmsg).textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                Button(action: {
+                    self.msg.addMsg(msg: self.typedmsg, user: self.name)
+                    self.typedmsg = ""
+                }) {
+                    
+                    Text("Send")
+                }
+                
+            }.padding()
+            
+        }
     }
+}
+
+class observer : ObservableObject {
+    @Published var msgs = [datatype]()
+    
+    init() {
+        let db = Firestore.firestore()
+        
+        db.collection("msgs").addSnapshotListener { (snap, err) in
+            
+            if err != nil {
+                print((err?.localizedDescription)!)
+                return
+            }
+            
+            for i in snap!.documentChanges {
+                if i.type == .added {
+                    let name = i.document.get("name") as! String
+                    let msg = i.document.get("msg") as! String
+                    let id = i.document.documentID
+                    
+                    self.msgs.append(datatype(id: id, name: name, msg: msg))
+                    
+                }
+            }
+            
+        }
+    }
+    
+    func addMsg(msg: String, user : String) {
+       let db = Firestore.firestore()
+        db.collection("msgs").addDocument(data: ["msg":msg, "name":user]) { (err) in
+            
+            if err != nil {
+                print((err?.localizedDescription)!)
+                return
+            }
+            print("success")
+            
+        }
+    }
+    
+    
+}
+
+struct datatype : Identifiable {
+    
+    var id : String
+    var name : String
+    var msg : String
+    
 }
